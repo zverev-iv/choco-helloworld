f9e0301abbe8691e19652b294c6bde60f01e57ec
Ilya Zverev <zverev.ivi@gmail.com>
2020-07-28 19:38:38 +0300

    init

---
+++ helloworld.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'helloworld*'
+  unzipLocation = $toolsDir
+  url           = $url
+  checksum      = ''
+  checksumType  = 'sha256'
+  url64bit      = $url64
+  checksum64    = ''
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
