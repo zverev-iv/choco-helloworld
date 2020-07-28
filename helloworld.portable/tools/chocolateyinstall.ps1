a57e7a388ec3f53857b1cb3e80fcc3d2e469307d
Ilya Zverev <zverev.ivi@gmail.com>
2020-07-28 21:06:17 +0300

    new package logic

--- helloworld.portable/tools/chocolateyinstall.ps1
+++ helloworld.portable/tools/chocolateyinstall.ps1
@@ -1,13 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$meta = Get-Content -Path $env:ChocolateyPackageFolder\tools\meta.json | ConvertFrom-Json
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'helloworld*'
-  unzipLocation = $toolsDir
-  url64bit      = 'https://github.com/zverev-iv/HelloWorld/releases/download/v0.0.1/HelloWorld.zip'
-  checksum64    = 'a718a69dc633f1ed70d4d76c3020c1d3a127b136cae93fb2c778f3e496dee8c0'
-  checksumType64= 'sha256'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  url64bit      = $meta.url64bit
+  checksum64    = $meta.checksum64
+  checksumType64= $meta.checksumType64
 }
 
 Install-ChocolateyZipPackage @packageArgs
