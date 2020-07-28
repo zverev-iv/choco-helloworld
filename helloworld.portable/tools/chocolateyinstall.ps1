08bc5ed3e35400a86ba583ee810da424fe26666b
Ilya Zverev <zverev.ivi@gmail.com>
2020-07-28 19:43:46 +0300

    fix package script

--- helloworld.portable/tools/chocolateyinstall.ps1
+++ helloworld.portable/tools/chocolateyinstall.ps1
@@ -5,11 +5,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'helloworld*'
   unzipLocation = $toolsDir
-  url           = $url
-  checksum      = ''
-  checksumType  = 'sha256'
-  url64bit      = $url64
-  checksum64    = ''
+  url64bit      = 'https://github.com/zverev-iv/HelloWorld/releases/download/v0.0.1/HelloWorld.zip'
+  checksum64    = 'a718a69dc633f1ed70d4d76c3020c1d3a127b136cae93fb2c778f3e496dee8c0'
   checksumType64= 'sha256'
 }
 
